88ce9586d5eabf1f439fbd94033af5881b13e17d
Radley <radley@alchemyandaim.com>
2023-06-21 22:39:48 -0700

    Removed JS from pdf

--- includes/pdf/preview.php
+++ includes/pdf/preview.php
@@ -23,9 +23,6 @@ class Class_AH_PDF_Preview {
 		$stylesheet = file_get_contents(__DIR__ . '/../../assets/pdf-preview.css');
 		$this->extra_css .= $stylesheet;
 		
-		$js = realpath(__DIR__ . '/../../assets/pdf.js');
-		$js = str_replace( ABSPATH, '/', $js );
-		
 		echo '<div class="previewpdf">';
 		echo $this->content;
 		echo '</div>';
@@ -35,11 +32,6 @@ class Class_AH_PDF_Preview {
 		echo $this->extra_css;
 		echo '</style>';
 		
-		// Load jquery and custom JS file
-		echo "\n\n";
-		echo '<script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>';
-		echo "\n\n";
-		echo '<script src="'. esc_attr($js) .'"></script>';
 		exit;
 	}
 	
